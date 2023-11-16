77a372c3ad5174b44650723dead51b81cae1903f
emaliemcmahon@gmail.com <emaliemcmahon@gmail.com>
2023-11-16 15:07:29 -0500

    model encoding

---
+++ scripts/sbatch_llm_encoding.sh
@@ -0,0 +1,17 @@
+#!/bin/bash -l
+
+#SBATCH
+#SBATCH --job-name=model_encoding
+#SBATCH --time=45:00
+#SBATCH --partition=a100
+#SBATCH --account=lisik3_gpu
+#SBATCH --nodes=1
+#SBATCH --cpus-per-task=6
+#SBATCH --gres=gpu:1
+#SBATCH --mail-type=end
+#SBATCH --mail-user=emcmaho7@jh.edu
+
+ml anaconda
+conda activate fmri_modeling
+
+python fmri_encoding.py
