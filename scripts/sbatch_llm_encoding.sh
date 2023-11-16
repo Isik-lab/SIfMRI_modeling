4aa4a446da4dce4a9721376f3efe9d67fef19549
emaliemcmahon@gmail.com <emaliemcmahon@gmail.com>
2023-11-16 15:30:56 -0500

    bug fix

--- scripts/sbatch_llm_encoding.sh
+++ scripts/sbatch_llm_encoding.sh
@@ -6,7 +6,8 @@
 #SBATCH --partition=a100
 #SBATCH --account=lisik3_gpu
 #SBATCH --nodes=1
-#SBATCH --mem=MaxMemPerNode
+#SBATCH --mem-per-cpu=10G
+#SBATCH --cpus-per-task=24
 #SBATCH --gres=gpu:1
 #SBATCH --mail-type=end
 #SBATCH --mail-user=emcmaho7@jh.edu
