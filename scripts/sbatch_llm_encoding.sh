f53853994607edb33247261c09f3106d3061fed4
emaliemcmahon@gmail.com <emaliemcmahon@gmail.com>
2023-11-30 11:30:28 -0500

    extraction

--- scripts/sbatch_llm_encoding.sh
+++ scripts/sbatch_llm_encoding.sh
@@ -1,20 +1,19 @@
 #!/bin/bash -l
 
 #SBATCH
-#SBATCH --job-name=model_encoding
-#SBATCH --time=45:00
+#SBATCH --job-name=testing
+#SBATCH --time=30:00
 #SBATCH --partition=a100
 #SBATCH --account=lisik3_gpu
 #SBATCH --nodes=1
 #SBATCH --mem-per-cpu=10G
-#SBATCH --cpus-per-task=8
-#SBATCH --gres=gpu:1
+#SBATCH --cpus-per-gpu=10
+#SBATCH --gres=gpu:2
+#SBATCH --exclusive
 #SBATCH --mail-type=end
 #SBATCH --mail-user=emcmaho7@jh.edu
-
-perturbation=$1
 
 ml anaconda
 conda activate fmri_modeling
 
-python llm_encoding.py --perturbation $perturbation --overwrite
+python llm_encoding.py --model_uid openai-gpt --overwrite
