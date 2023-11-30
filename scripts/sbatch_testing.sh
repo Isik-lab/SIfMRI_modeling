65c3bee3f4356f9323129aa14e6895eb5a50239b
emaliemcmahon@gmail.com <emaliemcmahon@gmail.com>
2023-11-30 11:15:22 -0500

    testing

--- scripts/sbatch_testing.sh
+++ scripts/sbatch_testing.sh
@@ -2,14 +2,13 @@
 
 #SBATCH
 #SBATCH --job-name=testing
-#SBATCH --time=15:00
+#SBATCH --time=1:00
 #SBATCH --partition=a100
 #SBATCH --account=lisik3_gpu
 #SBATCH --nodes=1
 #SBATCH --mem-per-cpu=10G
 #SBATCH --cpus-per-gpu=10
 #SBATCH --gpus-per-node=2
-#SBATCH --gres=gpu:2
 #SBATCH --exclusive
 #SBATCH --mail-type=end
 #SBATCH --mail-user=emcmaho7@jh.edu
