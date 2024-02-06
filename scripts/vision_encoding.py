515fa8a8bd15ee9980ee6cd654ebc5fe0d776536
emaliemcmahon@gmail.com <emaliemcmahon@gmail.com>
2024-02-06 08:59:57 -0500

    defaults

--- scripts/vision_encoding.py
+++ scripts/vision_encoding.py
@@ -87,7 +87,7 @@ def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('--model_uid', type=str, default='torchvision_alexnet_imagenet1k_v1')
     parser.add_argument('--overwrite', action=argparse.BooleanOptionalAction, default=False)
-    parser.add_argument('--save_frames', action=argparse.BooleanOptionalAction, default=True)
+    parser.add_argument('--save_frames', action=argparse.BooleanOptionalAction, default=False)
     parser.add_argument('--device', type=str, default='cuda:0')
     parser.add_argument('--data_dir', '-data', type=str,
                          default='/home/emcmaho7/scratch4-lisik3/emcmaho7/SIfMRI_modeling/data')                        
