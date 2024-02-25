08ad90f19e6ae7ecad6a66871ab49457dd5f72aa
garciakathy <kathyg@alumni.stanford.edu>
2024-02-25 15:34:01 -0500

    adding slack msg integration

--- scripts/rsa_benchmark.py
+++ scripts/rsa_benchmark.py
@@ -13,6 +13,8 @@ from deepjuice.model_zoo.options import get_deepjuice_model
 from deepjuice.procedural.datasets import get_image_loader
 from deepjuice.extraction import FeatureExtractor, get_feature_map_metadata
 from deepjuice.model_zoo import get_model_options
+from slack_sdk.webhook import WebhookClient
+from slack_sdk import WebClient
 
 class RSABenchmark:
     """
@@ -74,6 +76,27 @@ class RSABenchmark:
         stimulus_data_ = pd.read_csv(f'{self.data_dir}/interim/ReorganziefMRI/stimulus_data.csv')
         return Benchmark(metadata_, stimulus_data_, response_data_, rdms=True)
 
+    def send_slack(self, msg=None, filepath=None):
+        """
+         Helper function to send slack message to a webhook
+         Arguments:
+             msg: (str) The message to send. Defaults to None
+             filepath (str) Optionally included filepath to an attachment that you want to include. Defaults to None
+         Returns:
+             slack-sdk response
+        """
+        # Slack API functions
+        url = 'https://hooks.slack.com/services/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'
+        webhook = WebhookClient(url)
+        token = 'xoxp-508184378419-2331084514512-6070633594310-1c3ba2835c4bde49662d705517442b09'
+        client = WebClient(token)
+        response = None
+        if msg & filepath:
+            response = client.files_upload(channels='file_automation', title=filepath, file=filepath, initial_comment=msg)
+        elif msg:
+            response = webhook.send(text=msg)
+        return response
+
     def run(self):
         """
         Executes the RSA benchmarking process. This includes loading fMRI data, preparing stimulus data,
@@ -84,43 +107,39 @@ class RSABenchmark:
             print('Output file already exists. To run again pass --overwrite.')
             return
         else:
-            print('Loading data...')
-            benchmark = self.load_fmri()
-            stimulus_path = f'{self.data_dir}/raw/{self.model_input}/',
-            benchmark.add_stimulus_path(data_dir=stimulus_path, extension=self.extension)
-            benchmark.filter_stimulus(stimulus_set='train')
-
-            print('Loading model...')
-            model, preprocess = get_deepjuice_model(self.model_uid)
-            dataloader = get_image_loader(benchmark.stimulus_data['stimulus_path'], preprocess)
-            print('Extracting model features...')
-            feature_map_extractor = FeatureExtractor(model, dataloader,
-                                                     memory_limit='10GB',
-                                                     flatten=True,
-                                                     output_device='cuda',
-                                                     show_progress=True,
-                                                     exclude_oversize=True)
-            print('Model loaded!')
-            print('Extracting model layer metadata...')
-            feature_map_metadata = get_feature_map_metadata(model, dataloader, device='cuda', input_dim=0)
-            print('Model layers metadata loaded!')
-
-            print('Running rsa...')
-            results = encoding.get_training_rsa_benchmark_results(benchmark, feature_map_extractor, model_uid=self.model_uid)
-            print('Finished RSA scoring!')
-            results.to_csv(self.raw_out_file, index=False)
-            print(f'Raw results saved to {self.raw_out_file}')
-            results = pd.read_csv(self.raw_out_file)
-            print('Computing avg over folds')
-            results_avg = results.groupby(['model_uid', 'metric', 'cv_split', 'region', 'subj_id', 'model_layer', 'model_layer_index']).mean(numeric_only=True).reset_index()
-            crsa_results = results_avg[results_avg['metric'] == 'crsa']
-            ersa_results = results_avg[results_avg['metric'] == 'ersa']
-            print('Saving interim results...')
-            crsa_results.to_csv(self.crsa_out_file, index=False)
-            ersa_results.to_csv(self.ersa_out_file, index=False)
-            print('Finished interim results!')
-
             try:
+                print('Loading data...')
+                benchmark = self.load_fmri()
+                stimulus_path = f'{self.data_dir}/raw/{self.model_input}/',
+                benchmark.add_stimulus_path(data_dir=stimulus_path, extension=self.extension)
+                benchmark.filter_stimulus(stimulus_set='train')
+
+                print('Loading model...')
+                model, preprocess = get_deepjuice_model(self.model_uid)
+                dataloader = get_image_loader(benchmark.stimulus_data['stimulus_path'], preprocess)
+                print('Extracting model features...')
+                feature_map_extractor = FeatureExtractor(model, dataloader,
+                                                         memory_limit='10GB',
+                                                         flatten=True,
+                                                         output_device='cuda',
+                                                         show_progress=True,
+                                                         exclude_oversize=True)
+                print('Model loaded!')
+                print('Running rsa...')
+                results = encoding.get_training_rsa_benchmark_results(benchmark, feature_map_extractor, model_uid=self.model_uid)
+                print('Finished RSA scoring!')
+                results.to_csv(self.raw_out_file, index=False)
+                print(f'Raw results saved to {self.raw_out_file}')
+                results = pd.read_csv(self.raw_out_file)
+                print('Computing avg over folds')
+                results_avg = results.groupby(['model_uid', 'metric', 'cv_split', 'region', 'subj_id', 'model_layer', 'model_layer_index']).mean(numeric_only=True).reset_index()
+                crsa_results = results_avg[results_avg['metric'] == 'crsa']
+                ersa_results = results_avg[results_avg['metric'] == 'ersa']
+                print('Saving interim results...')
+                crsa_results.to_csv(self.crsa_out_file, index=False)
+                ersa_results.to_csv(self.ersa_out_file, index=False)
+                print('Finished interim results!')
+
                 print('Formatting results for plotting...')
                 for metric in ['crsa', 'ersa']:
                     columns = ['model_uid', 'region', 'model_layer', 'model_layer_index', 'subj_id', 'score']
@@ -159,7 +178,11 @@ class RSABenchmark:
                     df_result = df_result.sort_values(by='region')
                     df_result = df_result.reset_index(drop=True)
                     df_result['region'] = df_result['region'].str.upper()
+
                     # Add feature map metadata
+                    print('Extracting model layer metadata...')
+                    feature_map_metadata = get_feature_map_metadata(model, dataloader, device='cuda', input_dim=0)
+                    print('Model layers metadata loaded!')
                     df_result = df_result.merge(
                         feature_map_metadata[['output_uid', 'output_depth']].rename(
                             columns={'output_uid': 'model_layer', 'output_depth': 'Layer Depth'}),
@@ -177,12 +200,11 @@ class RSABenchmark:
                 elapsed = time.strftime("%H:%M:%S", time.gmtime(end_time - start_time))
                 print(f'Elapsed time: {elapsed}')
             except Exception as err:
-                print(f'Failed during formatting for plots with error msg: {err}')
-                print('Skipping formatting for plots.')
-                print('! Finished RSA Benchmark !')
+                print(f'Failed with error message: {err}')
                 end_time = time.time()
                 elapsed = time.strftime("%H:%M:%S", time.gmtime(end_time - start_time))
                 print(f'Elapsed time: {elapsed}')
+                self.send_slack(f'Model - {self.model_uid} - Failed with error message: {err}')
 
 
 def main():
