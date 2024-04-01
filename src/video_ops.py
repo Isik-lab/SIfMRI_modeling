9ad8e35dc380d34fc68dc66a470b42805055dd10
garciakathy <kathyg@alumni.stanford.edu>
2024-04-01 10:53:10 -0400

    xclip

--- src/video_ops.py
+++ src/video_ops.py
@@ -45,6 +45,9 @@ class VideoData(CustomData):
                 return_tensors="pt",
                 padding=True,
             )
+            inputs['input_ids'] = inputs['input_ids'].squeeze(0)
+            inputs['attention_mask'] = inputs['attention_mask'].squeeze(0)
+            inputs['pixel_values'] = inputs['pixel_values'].squeeze(0)
             inputs = inputs.to(self.device)
             return inputs
         else:
