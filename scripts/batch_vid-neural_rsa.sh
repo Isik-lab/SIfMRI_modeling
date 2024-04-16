bc9d6c42d284e5cb1ee07272cb3a4a5fbf77d7bf
garciakathy <kathyg@alumni.stanford.edu>
2024-04-16 09:54:18 -0400

    video array

--- scripts/batch_vid-neural_rsa.sh
+++ scripts/batch_vid-neural_rsa.sh
@@ -1,4 +1,12 @@
-#!/bin/bash -l
+#SBATCH
+#SBATCH --time=6:00:00
+#SBATCH --partition=ica100
+#SBATCH --account=lisik3_gpu
+#SBATCH --nodes=1
+#SBATCH --mem-per-cpu=10G
+#SBATCH --cpus-per-task=8
+#SBATCH --gres=gpu:1
+#SBATCH --output=slurm-%x-%j.out
 
 model=${1:-x3d-xs}
 
