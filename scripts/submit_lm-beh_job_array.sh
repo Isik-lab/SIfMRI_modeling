3082100debcf704a075deb5d4224ae219c15b85b
emaliemcmahon <emaliemcmahon@gmail.com>
2024-04-19 16:59:52 -0400

    lang perturb

---
+++ scripts/submit_lm-beh_job_array.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Parameters
+file="../data/raw/model_list/language_models.csv"
+funcs=(mask_adjectives mask_prepositions mask_nonadjectives mask_nonprepositions)
+num_funcs=${#funcs[@]}
+num_models=$(($(wc -l < "$file") - 1))  # Subtract 1 for the header
+echo "number of functions: $num_funcs" 
+echo "number of models: $num_models" 
+
+# Calculate total number of tasks
+total_tasks=$((num_models * num_funcs))
+echo "total number of tasks: " "$total_tasks" 
+
+# Submit the job array with the calculated total tasks
+sbatch --array=1-$total_tasks%6 batch_lm-beh_encoding_array.sh
