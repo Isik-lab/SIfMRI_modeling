0d5201bc0206f7933be0c0a6fad3b290395f2ef1
garciakathy <kathyg@alumni.stanford.edu>
2024-05-05 18:19:04 -0400

    behavior

--- src/behavior_alignment.py
+++ src/behavior_alignment.py
@@ -223,79 +223,88 @@ def get_video_benchmarking_results(benchmark, feature_extractor,
 
     layer_index = 0  # keeps track of depth
     results = []
-    for batch, feature_maps in enumerate(feature_extractor):
-        print(f"Running batch: {batch+1}")
+    extractor_iterator = tqdm(feature_extractor, desc='Extractor Steps')
+    for batched_feature_maps in extractor_iterator:
+        print(batched_feature_maps)
+        feature_maps = batched_feature_maps.join_batches()
         feature_map_iterator = tqdm(feature_maps.items(), desc='Brain Mapping (Layer)', leave=False)
         for feature_map_uid, feature_map in feature_map_iterator:
-            layer_index += 1 # one layer deeper in feature_maps
-
-            # reduce dimensionality of feature_maps by sparse random projection
-            feature_map = get_feature_map_srps(feature_map, device=devices[-1])
-            feature_map_info = {'model_uid': model_name,
-                                'model_layer': feature_map_uid,
-                                'model_layer_index': layer_index + layer_index_offset}
-
-            X = feature_map.detach().clone().squeeze().to(torch.float32).to(devices[-1])
-            X = {'train': X[indices['train']], 'test': X[indices['test']]}
-            del feature_map
-            torch.cuda.empty_cache()
+            layer_index += 1  # one layer deeper in feature_maps
 
             # Memory saving
             pipe = TorchRidgeGCV(alphas=alphas, alpha_per_target=True,
                                  device=devices[-1], scale_X=False)
+            try:
+                # reduce dimensionality of feature_maps by sparse random projection
+                feature_map = get_feature_map_srps(feature_map, device=devices[-1])
+                feature_map_info = {'model_uid': model_name,
+                                    'model_layer': feature_map_uid,
+                                    'model_layer_index': layer_index + layer_index_offset}
 
-            #### Fit CV in the train set ####
-            y_cv_pred, y_cv_true = [], []  # Initialize lists
-            for i, (cv_train_index, cv_test_index) in enumerate(cv.split(X['train'])):
-                # Split the training set
-                X_cv_train, X_cv_test = X['train'][cv_train_index].detach().clone(), X['train'][
-                    cv_test_index].detach().clone()
-                y_cv_train, y_cv_test = y['train'][cv_train_index].detach().clone(), y['train'][
-                    cv_test_index].detach().clone()
+                X = feature_map.detach().clone().squeeze().to(torch.float32).to(devices[-1])
+                X = {'train': X[indices['train']], 'test': X[indices['test']]}
+                del feature_map
+                torch.cuda.empty_cache()
+            except:
+                print(f'\nSRP memory too large {model_name} {feature_map_uid} ({layer_index + layer_index_offset})')
 
+            try:
+                #### Fit CV in the train set ####
+                y_cv_pred, y_cv_true = [], []  # Initialize lists
+                for i, (cv_train_index, cv_test_index) in enumerate(cv.split(X['train'])):
+                    # Split the training set
+                    X_cv_train, X_cv_test = X['train'][cv_train_index].detach().clone(), X['train'][
+                        cv_test_index].detach().clone()
+                    y_cv_train, y_cv_test = y['train'][cv_train_index].detach().clone(), y['train'][
+                        cv_test_index].detach().clone()
+
+                    # Scale X and y
+                    X_cv_train, X_cv_test = feature_scaler(X_cv_train, X_cv_test)
+                    if scale_y:
+                        y_cv_train, y_cv_test = feature_scaler(y_cv_train, y_cv_test)
+
+                    # Fit the regression
+                    pipe.fit(X_cv_train, y_cv_train)
+                    y_cv_pred.append(pipe.predict(X_cv_test))
+                    y_cv_true.append(y_cv_test)
+                scores_train = score_func(torch.cat(y_cv_pred), torch.cat(y_cv_true))  # Get the CV training scores
+
+                #### Fit the train/test split ####
                 # Scale X and y
-                X_cv_train, X_cv_test = feature_scaler(X_cv_train, X_cv_test)
+                X_train, X_test = feature_scaler(X['train'].detach().clone(), X['test'].detach().clone())
+                y_train, y_test = y['train'].detach().clone(), y['test'].detach().clone()
                 if scale_y:
-                    y_cv_train, y_cv_test = feature_scaler(y_cv_train, y_cv_test)
+                    y_train, y_test = feature_scaler(y_train, y_test)
 
                 # Fit the regression
-                pipe.fit(X_cv_train, y_cv_train)
-                y_cv_pred.append(pipe.predict(X_cv_test))
-                y_cv_true.append(y_cv_test)
-            scores_train = score_func(torch.cat(y_cv_pred), torch.cat(y_cv_true))  # Get the CV training scores
+                pipe.fit(X_train, y_train)
+                y_hat = pipe.predict(X_test)
+                scores_test = score_func(y_hat, y_test)
 
-            #### Fit the train/test split ####
-            # Scale X and y
-            X_train, X_test = feature_scaler(X['train'].detach().clone(), X['test'].detach().clone())
-            y_train, y_test = y['train'].detach().clone(), y['test'].detach().clone()
-            if scale_y:
-                y_train, y_test = feature_scaler(y_train, y_test)
+                r_null = stats.perm_gpu(y_test, y_hat)
+                r_var = stats.bootstrap_gpu(y_test, y_hat)
+                # Add performance to the score sheet
+                zip_iter = zip(target_features, scores_train.cpu().detach().numpy(),
+                               scores_test.cpu().detach().numpy(),
+                               r_null.cpu().detach().numpy().T,
+                               r_var.cpu().detach().numpy().T)
+                tqdm_iter = tqdm(zip_iter, total=len(target_features), desc="Reorg loop")
+                for target_feature, score_train, score_test, null, var in tqdm_iter:
+                    # add the scores to a "scoresheet"
+                    results.append({**feature_map_info,
+                                    'feature': target_feature,
+                                    'train_score': score_train,
+                                    'test_score': score_test,
+                                    'r_null_dist': null, 'r_var_dist': var})
 
-            # Fit the regression
-            pipe.fit(X_train, y_train)
-            y_hat = pipe.predict(X_test)
-            scores_test = score_func(y_hat, y_test)
-
-            r_null = stats.perm_gpu(y_test, y_hat)
-            r_var = stats.bootstrap_gpu(y_test, y_hat)
-            # Add performance to the score sheet
-            for target_feature, score_train, score_test, null, var in zip(target_features,
-                                                                          scores_train.cpu().detach().numpy(),
-                                                                          scores_test.cpu().detach().numpy(),
-                                                                          r_null.cpu().detach().numpy().T,
-                                                                          r_var.cpu().detach().numpy().T):
-                # add the scores to a "scoresheet"
-                results.append({**feature_map_info,
-                                'feature': target_feature,
-                                'train_score': score_train,
-                                'test_score': score_test,
-                                'r_null_dist': null, 'r_var_dist': var})
-
-            # Memory saving
-            del pipe, scores_train, scores_test
-            del X_cv_train, X_cv_test, y_cv_pred, y_cv_true
-            del X_train, X_test, y_train, y_test
-            del r_null, r_var
-            gc.collect()
-            torch.cuda.empty_cache()
+                # Memory saving
+                del pipe, scores_train, scores_test
+                del X_cv_train, X_cv_test, y_cv_pred, y_cv_true
+                del X_train, X_test, y_train, y_test
+                del r_null, r_var
+                gc.collect()
+                torch.cuda.empty_cache()
+            except:
+                print(
+                    f'\nFitting failed to converge for {model_name} {feature_map_uid} ({layer_index + layer_index_offset})')
     return pd.DataFrame(results)
