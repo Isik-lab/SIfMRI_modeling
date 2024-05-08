641f102e9c86a1110c54fb301d1992ce0d5283b0
emaliemcmahon <emaliemcmahon@gmail.com>
2024-05-08 14:15:03 -0400

    plotting

--- scripts/submit_lm-beh_job_array.sh
+++ scripts/submit_lm-beh_job_array.sh
@@ -13,4 +13,4 @@ total_tasks=$((num_models * num_funcs))
 echo "total number of tasks: " "$total_tasks" 
 
 # Submit the job array with the calculated total tasks
-sbatch --array=1-$total_tasks%1 batch_lm-beh_encoding_array.sh
+sbatch --array=1-$total_tasks%4 batch_lm-beh_encoding_array.sh
