eb0953be220f545907a63cab1e5affa5f92a337a
garciakathy <kathyg@alumni.stanford.edu>
2024-08-02 15:00:45 -0400

    filepaths

--- scripts/vision_neural_encoding.py
+++ scripts/vision_neural_encoding.py
@@ -153,7 +153,7 @@ class VisionNeuralEncoding:
 def main():
     parser = argparse.ArgumentParser()
     # Add arguments that are needed before setting the default for data_dir
-    parser.add_argument('--user', type=str, default='emcmaho7')
+    parser.add_argument('--user', type=str, default='kgarci18')
     # Parse known args first to get the user
     args, remaining_argv = parser.parse_known_args()
     user = args.user  # Get the user from the parsed known args
