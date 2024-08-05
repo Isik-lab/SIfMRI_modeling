80792dba5f340aeb9dfbf8a24c2ca7ac1cad4223
garciakathy <kathyg@alumni.stanford.edu>
2024-08-05 12:36:01 -0400

    mallm

--- src/video_ops.py
+++ src/video_ops.py
@@ -94,7 +94,7 @@ class VideoData(CustomData):
                 inputs = inputs.to(self.device)
             else:
                 inputs = [x.to(self.device) for x in inputs]
-            return {"image": inputs, "text_input": self.texts[index]}
+            return {"image": inputs, "text_input": 'People'}
 
         elif self.model_type == 'transformer':
             container = av.open(self.videos[index])
